c788ff0c5b1ebc3d22ef222621ef2f009dd2bb80
Youn Sung Huh <yh496@cornell.edu>
2020-02-15 14:16:08 -0500

    more fix whitenoise

--- allswap/settings.py
+++ allswap/settings.py
@@ -139,7 +139,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 REST_FRAMEWORK = {
