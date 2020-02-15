15b1daca5680cbfbdbbde7fcb1d0687c95e23d0e
Youn Sung Huh <yh496@cornell.edu>
2020-02-15 13:43:06 -0500

    ang

--- allswap/settings.py
+++ allswap/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIRS = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
