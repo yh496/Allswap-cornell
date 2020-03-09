0c38add1d5a556a07bbc173c08164b373649c30f
Jae Won Jung <jj393@cornell.edu>
2020-03-08 22:33:54 -0400

    signup, login, products frontend

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,74 +1,91 @@
-import React from 'react';
-import axios from 'axios';
-import { connect } from 'react-redux';
-import { Form, Input, Button } from 'antd';
+import React from "react";
+import axios from "axios";
+import { connect } from "react-redux";
+import { Form, Input, Button } from "antd";
 
 const FormItem = Form.Item;
 
-
 class CustomForm extends React.Component {
-    handleFormSubmit = (event, requestType, productID) => {
-        const title = event.target.elements.title.value;
-        const content = event.target.elements.content.value;
-        const price = event.target.elements.price.value;
-        const user = localStorage.getItem('user');
-        axios.defaults.headers = {
-            "Content-Type": "application/json",
-            Authorization: this.props.token
-        }
-        switch ( requestType ){
-            case 'post':
-                return axios.post('/api/', {
-                    user: user,
-                    title: title,
-                    content: content,
-                    price: price
-                })
-                .then(res => console.log(res))
-                .catch(error => console.err(error));
-            case 'put':
-                return axios.put(`/api/${productID}/`, {
-                    title: title,
-                    content: content,
-                    price: price
-                })
-                .then(res => console.log(res))
-                .catch(error => console.err(error));
-        }
-  
+  handleFormSubmit = (event, requestType, productID) => {
+    event.preventDefault();
+    const title = event.target.elements.title.value;
+    const content = event.target.elements.content.value;
+    const price = event.target.elements.price.value;
+    // const user = localStorage.getItem("user");
+    // console.log(user);
+    console.log(localStorage.getItem("token"));
+    console.log(this.props.token);
+    // axios.defaults.headers = {
+    //   "Content-Type": "application/json",
+    //   Authorization: this.props.token
+    // };
+
+    const config = {
+      headers: { Authorization: `Bearer ${this.props.token}` }
+    };
+    console.log("hi");
+    switch (requestType) {
+      case "POST":
+        return axios
+          .post(
+            "/api-products/",
+            {
+              // user: user,
+              title: title,
+              content: content,
+              price: price
+            },
+            config
+          )
+          .then(res => console.log(res))
+          .catch(error => console.err(error));
+      case "put":
+        return axios
+          .put(`/api/${productID}/`, {
+            title: title,
+            content: content,
+            price: price
+          })
+          .then(res => console.log(res))
+          .catch(error => console.err(error));
     }
-    render() {
-        
-        return (
-        <div>
-            <Form onSubmit = {(event) => this.handleFormSubmit(
-                event, 
-                this.props.requestType,
-                this.props.productID,
-                )}>
-            <FormItem label="Title">
-                <Input name = "title" placeholder="Put a title here" />
-            </FormItem>
-            <FormItem label="Content">
-                <Input name = "content" placeholder="Enter some content ..." />
-            </FormItem>
-            <FormItem label="Price">
-                <Input name = "price" placeholder="Enter some price ..." />
-            </FormItem>
-            <FormItem>
-                <Button type="primary" htmlType = "submit" >{this.props.btnTxt}</Button>
-            </FormItem>
-            </Form>
-        </div>
-        );
-    }
-}   
+  };
+  render() {
+    return (
+      <div>
+        <Form
+          onSubmit={event =>
+            this.handleFormSubmit(
+              event,
+              this.props.requestType,
+              this.props.productID
+            )
+          }
+        >
+          <FormItem label="Title">
+            <Input name="title" placeholder="Put a title here" />
+          </FormItem>
+          <FormItem label="Content">
+            <Input name="content" placeholder="Enter some content ..." />
+          </FormItem>
+          <FormItem label="Price">
+            <Input name="price" placeholder="Enter some price ..." />
+          </FormItem>
+          <FormItem>
+            <Button type="primary" htmlType="submit">
+              {this.props.btnTxt}
+            </Button>
+          </FormItem>
+        </Form>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = state => {
-    return {
-      token: state.token 
-    }
-  }
-
+  return {
+    token: state.token
+  };
+};
 
 export default connect(mapStateToProps)(CustomForm);
--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import ProductList from './containers/ProductListView';
-import ProductDetail from './containers/ProductDetailView';
-import Login from './containers/Login';
-import Signup from './containers/Signup';
-import Upload from './containers/ProductUploadView';
-import userProductList from './containers/userProductListView';
+import React from "react";
+import { Route } from "react-router-dom";
+import ProductList from "./containers/ProductListView";
+import ProductDetail from "./containers/ProductDetailView";
+import Login from "./containers/Login";
+import Signup from "./containers/Signup";
+import Upload from "./containers/ProductUploadView";
+import userProductList from "./containers/userProductListView";
 
 const BaseRouter = () => (
-    <div>
-        <Route exact path = '/' component = {ProductList} />
-        <Route exact path = '/myProducts/' component = {userProductList} />
-        <Route exact path = '/products/:productID/' component = {ProductDetail} />
-        <Route exact path = '/login/' component = {Login} />
-        <Route exact path = '/signup/' component = {Signup} />
-        <Route exact path = '/upload/' component = {Upload} />
-
-
-    </div>
+  <div>
+    <Route exact path="/" component={ProductList} />
+    <Route exact path="/myProducts/" component={userProductList} />
+    <Route exact path="/products/" component={ProductList} />
+    <Route exact path="/products/:productID/" component={ProductDetail} />
+    <Route exact path="/login/" component={Login} />
+    <Route exact path="/signup/" component={Signup} />
+    <Route exact path="/upload/" component={Upload} />
+  </div>
 );
 
 export default BaseRouter;
